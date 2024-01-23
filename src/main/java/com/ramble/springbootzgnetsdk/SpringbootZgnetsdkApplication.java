dc691ea767d7a2c9159b7c76d3cb3eb2823d8e72
陈 <cnaylor@163.com>
2024-01-23 11:44:48 +0800

    init  project

---
+++ src/main/java/com/ramble/springbootzgnetsdk/SpringbootZgnetsdkApplication.java
@@ -0,0 +1,13 @@
+package com.ramble.springbootzgnetsdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootZgnetsdkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootZgnetsdkApplication.class, args);
+    }
+
+}
